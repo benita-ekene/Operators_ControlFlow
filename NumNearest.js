c89ad48527f117fd21ca5416a7fc447ad9a60e17
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-02-18 22:49:42 +0100

    Answered question 5

--- NumNearest.js
+++ NumNearest.js
@@ -4,4 +4,4 @@ let pwr = 1;
 while (pwr <= num) {
   pwr *= 2;
 }
-  console.log(`The number ${pwr} is the power of 2 nearest to ${num}`);
+  console.log(`The number ${pwr} is the power of 2 nearest to the ${num}`);
