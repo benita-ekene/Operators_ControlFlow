f96313ad5fa26bd1932ff86d36d5e42ddd15ec9a
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-02-20 14:58:13 +0100

    Updated the answer to question 5

--- NumNearest.js
+++ NumNearest.js
@@ -1,9 +1,11 @@
-const num = 60; 
+const num = 20; 
 let pwr = 1;
 
 while (pwr <= num) {
   pwr *= 2;
 }
-
+if ((pwr - num) > (pwr / 2 - num)) {
+  pwr /= 2;
+}
 
 console.log('The number ' + pwr + ' is the power of 2 nearest to ' + num);
